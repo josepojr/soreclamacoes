af02ff2849902deed177e155e666b345fac4f969
josepojr <josepojr@172.16.0.160>
2017-12-12 14:40:53 -0200

    Configurações iniciais do projeto.

---
+++ soreclamacoes/src/main/java/com/example/demo/SoreclamacoesApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoreclamacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoreclamacoesApplication.class, args);
+	}
+}
